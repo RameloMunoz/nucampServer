66cffd113b99b26ca8f00b368f3cdcf322cd8333
Ramelo <RameloMunoz@gmail.com>
2021-05-21 20:11:16 -0700

    Passport JWT

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('passport');
-const authenticate = require('./authenticate');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
@@ -15,10 +11,11 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
-
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+
+const url = config.mongoUrl;
+
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -37,39 +34,14 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 
-function auth(req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-        const err = new Error('You are not authenticated!');                    
-        err.status = 401;
-        return next(err);
-    } else {
-        return next();
-    }
-}
-
-app.use(auth);
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
