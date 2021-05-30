8d90de261bf580026c1d2c478c1c8e5db01c6b64
Ramelo <RameloMunoz@gmail.com>
2021-05-29 19:22:51 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '544030030337927',
+        clientSecret: '94e821e6f458154e3e717cb473421d2f'
+    }
 }
