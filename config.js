0324a8ecd1961b637d9513f760de2da2249b6f3d
RameloMunoz <68353449+RameloMunoz@users.noreply.github.com>
2021-06-09 08:31:31 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '544030030337927',
-        clientSecret: '94e821e6f458154e3e717cb473421d2f'
-    }
-}
